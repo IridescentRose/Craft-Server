2dc1f18d6b859c38fbd25ac5e800b454998f5cd0
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-10-04 19:25:48 -0400

    Update build.zig

--- build.zig
+++ build.zig
@@ -13,15 +13,7 @@ const pkgs = struct {
 };
 
 pub fn build(b: *Builder) void {
-
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard release options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall.
     const mode = b.standardReleaseOptions();
 
     const exe = b.addExecutable("Craft-Server", "src/main.zig");
